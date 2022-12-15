0070c9279f87017366e78f33bbe854598fb22b5d
unknown <33-sohaibrousan>
2022-12-15 09:36:34 +0300

    sql

---
+++ task 14-DEC/task 14-DEC/WebForm1.aspx
@@ -0,0 +1,41 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="task_14_DEC.WebForm1" %>
+
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <!-- CSS only -->
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous"/>
+    <!-- JavaScript Bundle with Popper -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:Label ID="Label1" runat="server" Text="Label">Name</asp:Label>
+            <asp:TextBox ID="TextBox1" runat="server"></asp:TextBox><br />
+
+             <asp:Label ID="Label2" runat="server" Text="Label">age</asp:Label>
+            <asp:TextBox ID="TextBox2" runat="server"></asp:TextBox><br />
+
+             <asp:Label ID="Label3" runat="server" Text="Label">city</asp:Label>
+            <asp:TextBox ID="TextBox3" runat="server"></asp:TextBox><br />
+
+             <asp:Label ID="Label4" runat="server" Text="Label">phonenumber</asp:Label>
+            <asp:TextBox ID="TextBox4" runat="server"></asp:TextBox><br />
+
+
+            <asp:Label ID="Label5" runat="server" Text="Label">uploud your image</asp:Label>
+            <asp:FileUpload ID="FileUpload1" runat="server" />
+
+            <asp:Button ID="Button2" runat="server" Text="uploud" OnClick="Button1_Click" />
+            <br /><br />
+
+            <asp:Button ID="Button1" runat="server" Text="Button" OnClick="Button1_Click" />
+        </div>
+    </form>
+    <asp:Label ID="Label6" runat="server" Text="Label"></asp:Label>
+</body>
+</html>
---
+++ task 14-DEC/task 14-DEC/WebForm1.aspx.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.Ajax.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace task_14_DEC
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                SqlConnection con = new SqlConnection();
+                con.ConnectionString = "data source=DESKTOP-7JI0FAF\\SQLEXPRESS; database=task3; integrated security=SSPI";
+              con.Open();
+
+
+                string name = TextBox1.Text;
+                int age = Convert.ToInt32(TextBox2.Text);
+                string city= TextBox3.Text;
+                int phone= Convert.ToInt32(TextBox4.Text);
+                
+
+                string insert= "insert into customers (Name,age,city,phone_number) values('"+name+"','"+age+"','"+city+"','"+phone+"')";
+                SqlCommand cmd =new SqlCommand(insert,con);
+                cmd.ExecuteNonQuery();
+                con.Close();
+            }
+            catch (SqlException r)
+            {
+                Response.Write(r.Message);
+            }
+
+        }
+        private void usershow()
+        {
+          try
+            {
+                SqlConnection connectdata= new SqlConnection("data source=DESKTOP-7JI0FAF\\SQLEXPRESS; database=task3; integrated security=SSPI");
+                SqlCommand cmd2 = new SqlCommand("select * from customers", connectdata);
+                connectdata.Open();
+
+                SqlDataReader sdr= cmd2.ExecuteReader();
+                string tableusers = "<table class='table table-striped'> <tr><th>name</th> <th>age</th> <th>city</th> <th>phonenumber</th>";
+
+                while(sdr.Read())
+                {
+                    tableusers += $"<tr><td>{sdr[1]}</td> <td>{sdr[2]}</td>  <td>{sdr[3]}</td>  <td>{sdr[4]}</td> </tr>";
+                }
+                tableusers += "</table>";
+                Label6.Text = tableusers;
+                connectdata.Close();
+            }
+            catch(Exception w)
+            {
+                Label6.Text=w.Message;
+            }
+
+        }
+    }
+}
